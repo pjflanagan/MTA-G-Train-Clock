e4fc86788748c49d101fafa18ca0dfbaaddcf3b7
Peter James Flanagan <pjflanagan1@gmail.com>
2020-05-16 13:58:06 -0400

    calculate move time inbetween requests

--- src/mta-g-clock-arduino/mta-g-clock-arduino.ino
+++ src/mta-g-clock-arduino/mta-g-clock-arduino.ino
@@ -5,7 +5,10 @@
 #include "API.h"
 
 // when millis() is greater than this, it is time to make the next request
-int next_request_time = 0;
+int g_next_request_time = 0;
+
+// set this to millis() when you move the motors
+int g_last_move_time = 0;
 
 // time until next queens bound and brooklyn bound trains
 float qb, bb = 20;
@@ -18,9 +21,8 @@ void setup()
 
 void loop()
 {
-  int now = millis();
 
-  if (now > next_request_time)
+  if (millis() > g_next_request_time)
   {
     // if it is past the next request time, make a request
     String response = send_request();
@@ -28,15 +30,22 @@ void loop()
     Serial.println(response);
     qb = get_time_for_train('Q', &response);
     bb = get_time_for_train('B', &response);
+
+    // set the next time to make a request to 3/4 of the shortest train time
+    float request_delay = (qb < bb) ? qb : bb;
+    request_delay = request_delay * 60 * 1000;
+    g_next_request_time = millis() + int(request_delay) * 3 / 4;
   }
   else
   {
-    // otherwise subtract half a minute
-    qb = (qb <= 1) ? 0 : qb - .5;
-    bb = (bb <= 1) ? 0 : bb - .5;
+    // otherwise calculate the new wait time by subtracting how long it's been since the last request
+    float time_since_last_move_in_minutes = float((millis() - g_last_move_time)) / float(1000 * 60);
+    Serial.print("Time since last move: ");
+    Serial.println(time_since_last_move_in_minutes);
+    qb = (qb - time_since_last_move_in_minutes <= 0) ? 0 : qb - time_since_last_move_in_minutes;
+    bb = (bb - time_since_last_move_in_minutes <= 0) ? 0 : bb - time_since_last_move_in_minutes;
   }
 
-  // TODO: move the motors here
   Serial.print("Queens: ");
   Serial.println(qb);
   Serial.print("Brooklyn: ");
@@ -46,22 +55,21 @@ void loop()
   // qStepper.setDestinationMinutes(qb);
   // bStepper.setDestinationMinutes(bb);
 
-  // // move the motors
+  g_last_move_time = millis();
+  // // move the motors, this step takes time, so we will need to account for it when moving them the next time
   // while (!qStepper.done() && !bStepper.done())
   // {
   // 	qStepper.move();
   // 	bStepper.move();
   // 	delay(2);
   // }
+  //  TODO: calculate how long it took to move and see how long we need to delay int time_to_move = millis() - last_move_time;
 
-  // set the next time to make a request to 3/4 of the shortest train time
-  float delay_t = (qb < bb) ? qb : bb;
-  delay_t = delay_t * 60 * 1000;
-  next_request_time = now + int(delay_t) * 3 / 4;
+  Serial.print("Next request time: ");
+  Serial.println(g_next_request_time);
 
-  // delay 30 seconds
-  // TODO: this is wrong because moving takes time
-  delay(1000 * 30 * 50);
+  // delay 10 seconds
+  delay(1000 * 10);
 }
 
 #endif
